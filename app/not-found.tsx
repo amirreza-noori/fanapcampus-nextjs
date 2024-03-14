528e8a9a179fbfd46a885ccbbcdcf5abca393855
Amirreza Noori <hello@arnoori.ir>
2024-03-14 18:07:38 +0330

    return home added to not found page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function notfound() {
   return (
     <div>
         <h1>
             Page Not found
         </h1>
+        <Link href="/">Go Home</Link>
     </div>
   )
 }
